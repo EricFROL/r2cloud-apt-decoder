ea31903152d1b601c377029cab6d63106be0ed3d
ericfrol <42719382+EricFROL@users.noreply.github.com>
2022-12-18 00:24:16 +0100

    Update noaa18-cp.sh

--- noaa18-cp.sh
+++ noaa18-cp.sh
@@ -16,47 +16,28 @@ workday="$(date +"%d")"
 workhour="$(date +"%H")"
 workminute="$(date +"%M")"
 worksecond="$(date +"%S")"
-
-
-#condition statements
-
-
     if [ ! -e "/var/www/html/data/noaa/noaa18/donemarks/"$workdate"_done.mark" ]; then
-
-
             cd $datapath
             cd  "$(\ls -1dt ./*/ | head -n 1)"
-
             if [ ! -e "a.jpg" ]; then
-
                 echo "no valid data"
                 touch "/var/www/html/data/noaa/noaa18/donemarks/"$workdate"_done.mark"
-
-
             else
-
                 #folders build
                 mkdir /var/www/html/data/noaa/noaa18/"$workdate"
                 mkdir /var/www/html/data/noaa/noaa18/"$workdate"/decoded
-
                 #audio copy
                 cp ./output.wav /var/www/html/data/noaa/noaa18/"$workdate"/noaa18_"$workdate"_raw.wav
                 cd /var/www/html/data/noaa/noaa18/"$workdate"
-
-
                 #audio decoding
                 noaa-apt noaa18_"$workdate"_raw.wav -s noaa_18 -t "$workyear"-"$workmonth"-"$workday"T"$workhour":"$workminute":"$worksecond"$TZ -o ./decoded/noaa18_"$workdate"_default.png
                 noaa-apt noaa18_"$workdate"_raw.wav -s noaa_18 -t "$workyear"-"$workmonth"-"$workday"T"$workhour":"$workminute":"$worksecond"$TZ -F -c telemetry -o ./decoded/noaa18_"$workdate"_fcolort.png
                 noaa-apt noaa18_"$workdate"_raw.wav -s noaa_18 -t "$workyear"-"$workmonth"-"$workday"T"$workhour":"$workminute":"$worksecond"$TZ -F -c histogram -o ./decoded/noaa18_"$workdate"_fcolorhg.png 
                 noaa-apt noaa18_"$workdate"_raw.wav -s noaa_18 -t "$workyear"-"$workmonth"-"$workday"T"$workhour":"$workminute":"$worksecond"$TZ -F -c disable -o ./decoded/noaa18_"$workdate"_fcolor0c.png 
                 touch "/var/www/html/data/noaa/noaa18/donemarks/"$workdate"_done.mark"
-
             fi
-
     else
-
             echo "job already done"
             sleep 1200
     fi
-
 done
